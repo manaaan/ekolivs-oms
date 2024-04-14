bcacd5bd68ab5af74d857c16fe3fb2c417d05e55
Toni Hutter <hutter.toni@gmail.com>
2024-04-14 20:56:25 +0200

    28: add get products request to zettle api

--- backend/pkg/zettle/zettle.go
+++ backend/pkg/zettle/zettle.go
@@ -1,11 +1,36 @@
 package zettle
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 type Service struct{}
+
+const getProductsPath = "/organizations/{organizationUuid}/products/v2"
 
 func New() (*Service, error) {
 	return &Service{}, nil
 }
 
 func (Service) GetProducts() (ProductResponse, error) {
-	return ProductResponse{}, nil
+	response, err := http.Get(getProductsPath)
+	if err != nil {
+		fmt.Println(err)
+		return ProductResponse{}, err
+	}
+	defer response.Body.Close()
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ProductResponse{}, err
+	}
+
+	var products ProductResponse
+	json.Unmarshal(responseData, &products)
+
+	return products, nil
 }
