06ba8f02840e03eaf40d7b9b468455af89ccab89
Toni Hutter <hutter.toni@gmail.com>
2024-05-21 15:50:15 +0200

    28: add basic frontend setup

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,15 @@
+function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body>
+				<main>{children}</main>
+			</body>
+		</html>
+	);
+}
+
+export default RootLayout;
---
+++ frontend/app/page.tsx
@@ -0,0 +1,5 @@
+function Page() {
+	return <h1>Hello World</h1>;
+}
+
+export default Page;
