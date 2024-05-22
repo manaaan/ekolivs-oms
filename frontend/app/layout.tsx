d3fc04f5442708b87b5de7095afe77e04868db81
Toni Hutter <hutter.toni@gmail.com>
2024-05-22 22:20:34 +0200

    28: init shadcdn

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,21 @@
+import { cn } from '@/lib/utils'
+
+import { Inter as FontSans } from 'next/font/google'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
         <main>{children}</main>
       </body>
     </html>
