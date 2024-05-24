6675b0abc5e44c7fc4965f6189b2d3dbc0d3f72a
Toni Hutter <hutter.toni@gmail.com>
2024-05-24 16:30:35 +0200

    28: add mobile style for SidebarNav

--- frontend/components/SidebarNav/index.tsx
+++ frontend/components/SidebarNav/index.tsx
@@ -4,15 +4,15 @@ import SidebarItem from './SidebarItem'
 
 export const SidebarNav = () => {
   return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+    <aside className="fixed inset-x-0 z-10 flex min-h-14 items-center border-b bg-background sm:inset-y-0 sm:h-full sm:w-14 sm:flex-col sm:border-b-0 sm:border-r">
+      <nav className="flex items-center gap-4 px-2 sm:flex-col sm:py-5">
         <SidebarItem href={ROUTES.DASHBOARD} label="Home" />
         <SidebarItem href={ROUTES.ORDERS} label="Orders" />
         <SidebarItem href={ROUTES.DEMANDS} label="Demands" />
         <SidebarItem href={ROUTES.PRODUCTS} label="Products" />
         <SidebarItem href={ROUTES.CUSTOMERS} label="Customers" />
       </nav>
-      <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+      <nav className="ml-auto gap-4 px-2 sm:ml-0 sm:mt-auto sm:items-center sm:py-5">
         <SidebarItem href={ROUTES.SETTINGS} label="Settings" />
       </nav>
     </aside>
