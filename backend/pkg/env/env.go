b2454182b737ef658583e4ba365fa2d5e42f2f50
Pit Müller <pit.mueller@ingka.ikea.com>
2025-04-15 13:24:30 +0200

    chore: dockerize product service
    Not fully functioning, as it requires authentication to gcloud to be
    injected for local deployment.
    Maybe replace with local DB instead, as previosuly discussed to avoid
    local dev requirement to be online.

--- backend/pkg/env/env.go
+++ backend/pkg/env/env.go
@@ -19,6 +19,7 @@ func Required(e string) string {
 func LoadEnv() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Unable to load .env file")
+		// If injected on runtime, this is not an issue
+		log.Println("Unable to load .env file")
 	}
 }
