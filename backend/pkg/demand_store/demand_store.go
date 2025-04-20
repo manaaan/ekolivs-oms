8300008d9cb4d50d6d65b208f8a6bf1ace2ef26f
Toni Hutter <hutter.toni@gmail.com>
2025-04-20 16:35:10 +0200

    2: add demand service

--- backend/pkg/demand_store/demand_store.go
+++ backend/pkg/demand_store/demand_store.go
@@ -19,12 +19,9 @@ type StorePosition struct {
 	api.Position
 }
 
-type StoreDemand struct {
-	api.Demand
-}
-
-func (s Store) GetDemands(ctx context.Context) ([]*StoreDemand, error) {
-	var demands []*StoreDemand
+// TODO: join positions with demands
+func (s Store) GetDemands(ctx context.Context) ([]*api.Demand, error) {
+	var demands []*api.Demand
 	iter := s.FirestoreClient.Collection(collection).OrderBy("ID", firestore.Asc).Documents(ctx)
 	defer iter.Stop()
 	for {
@@ -35,7 +32,7 @@ func (s Store) GetDemands(ctx context.Context) ([]*StoreDemand, error) {
 		if err != nil {
 			return nil, err
 		}
-		var prod StoreDemand
+		var prod api.Demand
 		if err := dsnap.DataTo(&prod); err != nil {
 			return nil, err
 		}
@@ -44,13 +41,22 @@ func (s Store) GetDemands(ctx context.Context) ([]*StoreDemand, error) {
 	return demands, nil
 }
 
-func (s Store) CreateDemand(ctx context.Context, demand *StoreDemand) (*StoreDemand, error) {
-	if _, err := s.FirestoreClient.Collection(collection).Doc(demand.ID).Set(ctx, demand); err != nil {
-		return nil, err
-	}
-	return demand, nil
+// TODO: create connected positions
+func (s Store) CreateDemand(ctx context.Context, create *api.CreateDemandReq) (*api.Demand, error) {
+	//if _, err := s.FirestoreClient.Collection(collection)
+	//	return nil, err
+	//}
+	//return demand, nil
+	return &api.Demand{
+		ID:          "",
+		Positions:   create.Positions,
+		Status:      0,
+		FulfilledAt: nil,
+		CreatedAt:   "",
+	}, nil
 }
 
+// TODO: delete connected positions
 func (s Store) DeleteDemand(ctx context.Context, id string) error {
 	if _, err := s.FirestoreClient.Collection(collection).Doc(id).Delete(ctx); err != nil {
 		return err
---
+++ backend/services/demand/internal/demand/demand.go
@@ -0,0 +1,50 @@
+package demand
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"github.com/manaaan/ekolivs-oms/demand/api"
+	"github.com/manaaan/ekolivs-oms/pkg/demand_store"
+	"log/slog"
+)
+
+type Service struct {
+	storeService *demand_store.Store
+}
+
+func New(firestoreClient *firestore.Client) *Service {
+	return &Service{
+		storeService: &demand_store.Store{
+			FirestoreClient: firestoreClient,
+		},
+	}
+}
+
+func (s Service) GetDemands(ctx context.Context) ([]*api.Demand, error) {
+	demands, err := s.storeService.GetDemands(ctx)
+	if err != nil {
+		slog.Error("failed to get demands from demand store", "error", err)
+		return nil, err
+	}
+
+	return demands, nil
+}
+
+func (s Service) CreateDemand(ctx context.Context, create *api.CreateDemandReq) (*api.Demand, error) {
+	demand, err := s.storeService.CreateDemand(ctx, create)
+	if err != nil {
+		slog.Error("failed to create demand", "error", err)
+		return nil, err
+	}
+
+	return demand, nil
+}
+
+func (s Service) DeleteDemand(ctx context.Context, id string) error {
+	err := s.storeService.DeleteDemand(ctx, id)
+	if err != nil {
+		slog.Error("failed to delete demand", "error", err)
+		return err
+	}
+	return nil
+}
