f90e39133c9aa1b0d00d21d76236d27e226bf762
Toni Hutter <hutter.toni@gmail.com>
2025-05-03 16:20:31 +0200

    2: Add demand position functions to demand_store.go

--- backend/pkg/demand_store/demand_store.go
+++ backend/pkg/demand_store/demand_store.go
@@ -19,10 +19,9 @@ type StorePosition struct {
 	api.Position
 }
 
-// TODO: join positions with demands
 func (s Store) GetDemands(ctx context.Context) ([]*api.Demand, error) {
 	var demands []*api.Demand
-	iter := s.FirestoreClient.Collection(collection).OrderBy("ID", firestore.Asc).Documents(ctx)
+	iter := s.FirestoreClient.Collection(collection).Documents(ctx)
 	defer iter.Stop()
 	for {
 		dsnap, err := iter.Next()
@@ -32,21 +31,18 @@ func (s Store) GetDemands(ctx context.Context) ([]*api.Demand, error) {
 		if err != nil {
 			return nil, err
 		}
-		var prod api.Demand
-		if err := dsnap.DataTo(&prod); err != nil {
+		var demand api.Demand
+		if err := dsnap.DataTo(&demand); err != nil {
 			return nil, err
 		}
-		demands = append(demands, &prod)
+		demands = append(demands, &demand)
 	}
 	return demands, nil
 }
 
 // TODO: create connected positions
+// Mocked response
 func (s Store) CreateDemand(ctx context.Context, create *api.CreateDemandReq) (*api.Demand, error) {
-	//if _, err := s.FirestoreClient.Collection(collection)
-	//	return nil, err
-	//}
-	//return demand, nil
 	return &api.Demand{
 		ID:          "",
 		Positions:   create.Positions,
@@ -56,10 +52,39 @@ func (s Store) CreateDemand(ctx context.Context, create *api.CreateDemandReq) (*
 	}, nil
 }
 
-// TODO: delete connected positions
 func (s Store) DeleteDemand(ctx context.Context, id string) error {
 	if _, err := s.FirestoreClient.Collection(collection).Doc(id).Delete(ctx); err != nil {
 		return err
 	}
 	return nil
+}
+
+// TODO: Should this stay here or in a demand_postition_store.go?
+func (s Store) GetDemandPositions(ctx context.Context, productId string) ([]*api.Position, error) {
+	var positions []*api.Position
+	iter := s.FirestoreClient.Collection("positions").Where("ProductId", "==", productId).Documents(ctx)
+	defer iter.Stop()
+	for {
+		dsnap, err := iter.Next()
+		if errors.Is(err, iterator.Done) {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		var prod api.Position
+		if err := dsnap.DataTo(&prod); err != nil {
+			return nil, err
+		}
+		positions = append(positions, &prod)
+	}
+	return positions, nil
+}
+
+func (s Store) CreateDemandPosition(ctx context.Context, position *api.Position) (*api.Position, error) {
+	if _, _, err := s.FirestoreClient.Collection("positions").Add(ctx, position); err != nil {
+		return nil, err
+	}
+
+	return position, nil
 }
